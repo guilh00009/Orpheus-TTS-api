c37daf364c8461c6730fc9585bfabe1f4e6a7a79
GUILHERME KELLER <121647297+guilh00009@users.noreply.github.com>
2025-03-20 12:08:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -229,5 +229,5 @@ def get_emotions():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port, threaded=True) 
